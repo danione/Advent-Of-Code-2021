d7d1fa1d308b438de5c7c148a21f33e8eb00de9f
danione <yordan.yankulov@icloud.com>
2022-03-19 21:56:33 +0000

    Day 6 Optimisation
    Part 2 was taking way too long. Therefore, decided to implement a map object and sum only.

--- Day 6/LanternFishSimulator.cpp
+++ Day 6/LanternFishSimulator.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <unordered_map>
+#include <map>
 using namespace std;
 
 void printTimers(vector<int>& lanternFish){
@@ -24,31 +26,128 @@ void populateLanternFish(vector<int>& lanternFish){
   }
 }
 
-void dayIterate(vector<int>& lanternFish, int days){
+// void dayIterate(vector<int>& lanternFish, int days){
+//   int day = 1;
+//   while(day <= days){
+//     //cout << "After " << day << ((day == 1) ? " day: " : " days: ");
+//     int sizePre = lanternFish.size();
+//     for(int i = 0; i < sizePre; i++){
+//       //cout << lanternFish[i] << ((i+1 < lanternFish.size())? "," : "");
+//       if(lanternFish[i] > 0)
+//         lanternFish[i]--;
+//       else{
+//         lanternFish[i] = 6;
+//         lanternFish.push_back(8);
+//       }
+//     }
+//     //cout << endl;
+//     day++;
+//   }
+// }
+
+long unsigned dayIterateIt(vector<int>& lanternFish, int days){
   int day = 1;
-  while(day <= days){
-    cout << "After " << day << ((day == 1) ? " day: " : " days: ");
-    int sizePre = lanternFish.size();
-    for(int i = 0; i < sizePre; i++){
-      cout << lanternFish[i] << ((i+1 < lanternFish.size())? "," : "");
-      if(lanternFish[i] > 0)
-        lanternFish[i]--;
-      else{
-        lanternFish[i] = 6;
-        lanternFish.push_back(8);
-      }
+  long unsigned size = lanternFish.size();
+  unordered_map <int,vector<int>> dayIterator;
+  for(int i = 0; i < lanternFish.size(); i++){
+    dayIterator[lanternFish[i]].push_back(i);
+  }
+
+  while(day < days){
+    for(int i = 0; i < dayIterator[day].size(); i++){
+      int currentFish = dayIterator[day][i];
+      dayIterator[day + 7].push_back(currentFish);
+      size++;
+      dayIterator[day + 9].push_back(size);
     }
-    cout << endl;
+    dayIterator.erase(day);
     day++;
   }
+
+  return size;
 }
+
+// long unsigned dayIterate(vector<int>& lanternFish, int days){
+//   int day = 1;
+//   long unsigned size = lanternFish.size();
+//   unordered_map <int,int> dayIterator;
+//   for(int i = 0; i < lanternFish.size(); i++){
+//     if(dayIterator[lanternFish[i]]){
+//       dayIterator[lanternFish[i]]++;
+//     } else
+//       dayIterator[lanternFish[i]] = 1;
+//   }
+//
+//   while(day < days){
+//     for(int i = 0; i < dayIterator[day]; i++){
+//       if(dayIterator[day + 7]){
+//         dayIterator[day + 7]++;
+//       } else
+//         dayIterator[day + 7] = 1;
+//       size++;
+//       if(dayIterator[day + 9]){
+//         dayIterator[day + 9]++;
+//       } else
+//         dayIterator[day + 9] = 1;
+//     }
+//     dayIterator.erase(day);
+//     day++;
+//   }
+//
+//   return size;
+// }
+
+void init(map<int, long unsigned>& tracker, vector<int> lanternFish){
+  for(int i = 0; i <= 8; i++){
+    tracker[i] = 0;
+  }
+  for(int i = 0; i < lanternFish.size(); i++){
+    tracker[lanternFish[i]]++;
+  }
+}
+
+void print(map<int, long unsigned>& tracker){
+  for(int i = 0; i <= 8; i++){
+    cout << tracker[i] << ", ";
+  }
+  cout << endl;
+
+}
+
+long unsigned sum(map<int, long unsigned>& tracker){
+  long unsigned sum = 0;
+  for(int i = 0; i <= 8; i++){
+    sum += tracker[i];
+  }
+  return sum;
+}
+
+long unsigned dayIterate(vector<int>& lanternFish, int days){
+  map<int, long unsigned> tracker;
+  int day = 1;
+  init(tracker, lanternFish);
+  long unsigned lastFront = 0;
+  while(day < days){
+    for(int i = 0; i <= 7; i++){
+      swap(tracker[i], tracker[i+1]);
+    }
+    tracker[7] += tracker[0];
+    lastFront = tracker[0];
+    day++;
+  }
+  return sum(tracker);
+}
+
+
 
 int main(){
   vector<int> lanternFish;
   populateLanternFish(lanternFish);
-  cout << "Initial state: ";
-  printTimers(lanternFish);
-  dayIterate(lanternFish, 80);
-  cout << lanternFish.size() << endl;
+  // cout << "Initial state: ";
+  // printTimers(lanternFish);
+
+  cout << dayIterate(lanternFish, 256) << endl;
+  // cout << dayIterateIt(lanternFish, 80) << endl;
+
   return 0;
 }
