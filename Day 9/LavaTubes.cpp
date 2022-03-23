878a08687b827b402a0950294d49a06ab67d3c39
danione <yordan.yankulov@icloud.com>
2022-03-23 22:37:22 +0000

    Day 9 Part 2 Completed
    Finished it. Looks a bit ugly but workable... Love the using of the set and recursion. Got to learn a bit from that tho.

--- Day 9/LavaTubes.cpp
+++ Day 9/LavaTubes.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unordered_set>
 using namespace std;
 
 bool checkIfLowestValue(int row, int column, vector<vector<char>>& input){
@@ -54,16 +55,17 @@ bool checkIfLowestValue(int row, int column, vector<vector<char>>& input){
     return 1;
 }
 
-void checkVectorInput(vector<vector<char>>& input){
+void checkVectorInput(vector<vector<char>>& input,unordered_set<string>& source){
   int sum = 0;
   for(int row = 0; row < input.size(); row++){
     for(int column = 0; column < input[row].size(); column++){
       if(checkIfLowestValue(row, column, input) == 1){
-        sum += (input[row][column] - '0') + 1;
+        //sum += (input[row][column] - '0') + 1;
+        source.insert(to_string(row) + ',' + to_string(column));
       }
     }
   }
-  cout << sum << endl;
+  // cout << sum << endl;
 }
 
 void handleInput(vector<vector<char>>& input){
@@ -80,9 +82,42 @@ void handleInput(vector<vector<char>>& input){
   }
 }
 
+void basin(int row, int column,vector<vector<char>>& input, unordered_set<string>& source){
+  string key = to_string(row) + ',' + to_string(column);
+  if(input[row][column] == '9' || source.find(key) != source.end())
+    return;
+
+  source.insert(key);
+
+  if(row + 1 <= input.size() - 1)
+    basin(row + 1,column, input, source);
+  if(row - 1 >= 0)
+    basin(row - 1,column, input, source);
+  if(column + 1 <= input[row].size() - 1)
+    basin(row, column+ 1, input, source);
+  if(column - 1 >= 0)
+    basin(row, column- 1, input, source);
+
+}
+
 int main(){
   vector<vector<char>> input;
+  unordered_set<string> source;
+  vector<int> sizes;
   handleInput(input);
-  checkVectorInput(input);
+  checkVectorInput(input, source);
+  for (const auto& elem: source) {
+    unordered_set<string> basinHold;
+
+    basin(stoi(elem.substr(0,elem.find(','))),stoi(elem.substr(elem.find(',')+1,elem.length())),input, basinHold);
+    sizes.push_back(basinHold.size());
+  }
+  sort(sizes.begin(), sizes.end());
+  int sum = sizes[sizes.size() - 1];
+  cout << sum<< endl;
+  for(int i = sizes.size() - 2; i > sizes.size() - 4; i--){
+    sum*=sizes[i];
+  }
+  cout << sum << endl;
   return 0;
 }
