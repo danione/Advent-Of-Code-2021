6d855e1515240c575c3ec722fcece08bef41ffc6
danione <yordan.yankulov@icloud.com>
2022-04-16 10:20:00 +0100

    Day 15 Part 2
    Day 15 is finished. However, needs to be optimised. Perhaps priority queue can be used.

--- Day 15/Nav.cpp
+++ Day 15/Nav.cpp
@@ -48,30 +48,48 @@ class Graph{
     }
 
     void addExtraEdges(){
-      for(int i = 0; i < lRow; i++){
-        for(int j = i + lRow; j < lRow*lRow; j += lRow){
+      cout << "Adding extra edges..." << endl;
+      for(int i = 0; i < lRow; i++)
+        for(int j = i + lRow; j < lRow*lRow; j += lRow)
           addEdge(j, j-lRow);
-        }
-      }
+
+      cout << "Finished!" << endl;
     }
 
     void print(){
       for(int i = 0; i < adjList[4].size(); i++)
-      {
         cout << adjList[4][i].first.index << ' ' << adjList[4][i].second << endl;
-      }
+    }
 
+    void dissect(string line, int offset, int times = 5){
+      int time = 0;
+      while(time < times){
+        for(int i = 0; i < line.length(); i++){
+          int charToInt = line[i] + offset + time;
+          if(charToInt <= '9')
+            addNode(char(charToInt));
+          else
+            addNode((char)((charToInt - '0') % 10 + 1 + '0'));
+        }
+        time++;
+      }
     }
 
     void init(){
+      const int grid = 5;
       ifstream infile("input.txt");
-      string value;
-      while(getline(infile, value)){
-        for(int i = 0; i < value.length(); i++)
-          addNode(value[i]);
-      }
-    }
+      string val;
+      vector<string> value;
+      cout << "Initing..." << endl;
 
+      while(getline(infile, val))
+        value.push_back(val);
+      for(int i = 0; i < grid; i++)
+        for(int j = 0; j < value.size(); j++)
+          dissect(value[j], i);
+      cout << "Finished!" << endl;
+
+    }
 
     void runDistanceAlgo(deque<Node>& que, vector<int>& distance){
       set<long long unsigned> visited;
@@ -80,6 +98,7 @@ class Graph{
         auto comp = [&](Node a, Node b)-> bool {
           return distance[a.index] < distance[b.index];
         };
+        cout << que.size() - 1 << " elements left" << endl;
         auto nextEl = min_element(que.begin(), que.end(), comp);
         long long unsigned next = nextEl->index;
 
@@ -103,19 +122,28 @@ class Graph{
       vector<int> distance(nodes.size());
       deque<Node> que;
 
+      cout << "Dijkstra initing..." << endl;
+
+
       if(start >= 0 && start < nodes.size()){
         distance[start] = 0;
         que.push_back(nodes[start]);
       }
 
+      cout << "Calculating..." << endl;
       for(int i = 0; i < nodes.size(); i++){
         if(i != start){
           distance[i] = INT_MAX;
           que.push_back(nodes[i]);
         }
+        cout << i + 1 << "out of " << nodes.size() << " calculated" << endl;
       }
 
+      cout << "Running distance algo..." << endl;
+
       runDistanceAlgo(que, distance);
+      cout << "Finished!" << endl;
+
     }
 };
 
@@ -124,7 +152,7 @@ int main(){
   ifstream infile("input.txt");
   string value;
   getline(infile,value);
-  Graph* gr = new Graph(value.length());
+  Graph* gr = new Graph(value.length() * 5);
   infile.close();
 
   gr->init();
